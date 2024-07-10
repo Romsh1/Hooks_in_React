b7f95b042a00d3e839d7d38be06b07553bafb240
Romika <romika2002chau.21@gmail.com>
2024-07-10 13:43:51 -0700

    Learnt about dispatch function - Day 86 of Learning React and JS

--- react-quiz/src/DateCounter.js
+++ react-quiz/src/DateCounter.js
@@ -2,8 +2,8 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  if(action.type === 'inc') return state + action.payload;
-  if(action.type === 'dec') return state - action.payload;
+  if(action.type === 'inc') return state + 1;
+  if(action.type === 'dec') return state - 1;
   if (action.type === 'setCount') return action.payload; 
 }
 
@@ -19,20 +19,20 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch({type: "dec", payload: -1});
+    dispatch({type: "dec"});
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch({type: "inc", payload: 1});
+    dispatch({type: "inc"});
 
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    dispatch(Number(e.target.value));
+    dispatch({ type: "setCount", payload:  Number(e.target.value)});
     // setCount(Number(e.target.value));
   };
 
