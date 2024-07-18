95e1564cc33268a15c03f1240f7ad98f085ef134
Romika <romika2002chau.21@gmail.com>
2024-07-18 13:02:28 -0700

    Used useReducer hook to store data

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -2,14 +2,45 @@
 
 import Header from "./Header";
 import Main from "./Main";
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case 'dataFailed':
+      return {
+        ...state,
+        state: "error",
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+//useReducer to store that data
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //Used useEffect to fetch data on an initial render
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"));
+      .then((data) => dispatch({type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({type: 'dataFailed'}));
   }, []);
 
   return ( 
